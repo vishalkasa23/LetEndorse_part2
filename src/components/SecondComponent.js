f3165bd4b9afd6d828ef74b97717fa1e3ea1dde0
Vishal Kasa <88143144+vishalkasa23@users.noreply.github.com>
2023-03-16 13:29:34 +0530

    removed warnings

--- src/components/SecondComponent.js
+++ src/components/SecondComponent.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import myImage from '../assets/Offer_expectation.png';
 const SecondComponent = () => {
